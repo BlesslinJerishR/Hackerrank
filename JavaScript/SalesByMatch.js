7354abd35e49477104294ed6e72b08ebb77f7d74
Mastermind <mastermindx33@gmail.com>
2023-01-17 23:14:16 +0530

    SalesByMatch

--- JavaScript/SalesByMatch.js
+++ JavaScript/SalesByMatch.js
@@ -1,7 +1,7 @@
 function sockMerchant(n, ar) {
     var pair = 0;
         ar.sort();
-        console.log(ar.sort());
+        //console.log(ar.sort());
         for(let i=0; i<n;i++){
             if(ar[i] == ar[i+1]){
                 i++;
@@ -10,6 +10,20 @@ function sockMerchant(n, ar) {
         }
         // console.log(pair);
     return pair;
+}
+
+
+function sockMerchant(n, ar) {
+    // Write your code here
+    let pairs = 0;
+    ar.sort();
+    for(let sock = 0; sock < ar.length; sock++){
+        if(ar[sock] === ar[sock + 1]){
+            pairs++;
+            sock++;
+        }
+    }
+    return pairs;
 }
 
 // sockMerchant(10,[
