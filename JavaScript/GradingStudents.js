0e0631224a4b230ad36c9b195636e40e34c74923
Mastermind <mastermindx33@gmail.com>
2023-01-17 23:08:41 +0530

    Grading Students

--- JavaScript/GradingStudents.js
+++ JavaScript/GradingStudents.js
@@ -1,6 +1,19 @@
 function gradingStudents(grades){
+    // Write your code here
     let finalGrades = grades.map((grade) =>{
         return grade >= 38 && grade % 5 >= 3 ? grade - (grade %5) + 5: grade;
     });
     return finalGrades;
+}
+
+// Without Ternary Operator
+function gradingStudents(grades) {
+    // Write your code here
+    return grades.map((grade) =>{
+        if(grade >= 38 && grade % 5 >= 3){
+            return grade - grade % 5 + 5;
+        }else{
+            return grade;   
+        }
+    })
 }
