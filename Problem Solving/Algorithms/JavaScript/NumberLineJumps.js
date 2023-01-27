014653b82e040b4bb24f543d6cb1e2c509ff1b2f
Mastermind <mastermindx33@gmail.com>
2023-01-27 18:39:45 +0530

    Number Line Jumps Bit

--- Problem Solving/Algorithms/JavaScript/NumberLineJumps.js
+++ Problem Solving/Algorithms/JavaScript/NumberLineJumps.js
@@ -6,3 +6,15 @@ function kangaroo(x1, v1, x2, v2) {
 
 // Test run:
 console.log(kangaroo(0, 2, 5, 3));
+
+// Bit
+
+function kangaroo(x1, v1, x2, v2) {
+    if (v1 < v2)
+      return "NO";
+
+    if ((x2 - x1) % (v1 - v2) == 0)
+      return "YES";
+    else
+      return "NO";
+  }
